2cc431d4cb5e007cb08bea4654e209303a347beb
Your Name <mostafaabdalla779933@gmail.com>
2021-06-23 14:24:00 +0200

    feat: add splash screen

---
+++ app/src/main/java/com/example/task_vodafone/SplashActivity.kt
@@ -0,0 +1,35 @@
+package com.example.task_vodafone
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.task_vodafone.databinding.ActivitySplashBinding
+import kotlinx.coroutines.*
+
+class SplashActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivitySplashBinding
+    lateinit var job : Job
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        job = CoroutineScope(Dispatchers.Main).launch {
+
+            delay(1000)
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+
+        }
+
+
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        job.cancel()
+
+    }
+}
